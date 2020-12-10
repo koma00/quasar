80f9f52e1e2c364c62e5ea8857b755feb3c12122
koma00 <koma199604@gmail.com>
2020-12-10 23:18:10 +0200

    Added friends page

---
+++ src/pages/Friends.vue
@@ -0,0 +1,99 @@
+<template>
+  <q-page padding>
+    <div id="q-app">
+      <div class="q-pa-md q-gutter-md text-primary">
+        <q-list bordered class="rounded-borders">
+          <q-item-label header>Friends</q-item-label>
+
+          <q-item clickable v-ripple @click="$router.replace('profile/1')">
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/avatar1.jpg">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label lines="1">Miho Seko</q-item-label>
+              <q-item-label caption lines="1">
+                Online
+              </q-item-label>
+            </q-item-section>
+
+            <q-item-section side @click="$router.replace('chat/1')">
+              <q-btn size="10px" color="primary" label="Send message"></q-btn>
+            </q-item-section>
+          </q-item>
+
+          <q-separator inset="item"></q-separator>
+
+          <q-item clickable v-ripple @click="$router.replace('chat/1')">
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label lines="1">Mary Ride</q-item-label>
+              <q-item-label caption lines="1">
+                Online
+              </q-item-label>
+            </q-item-section>
+
+            <q-item-section side @click="$router.replace('chat/1')">
+              <q-btn size="10px" color="primary" label="Send message"></q-btn>
+            </q-item-section>
+          </q-item>
+
+          <q-separator inset="item"></q-separator>
+
+          <q-item clickable v-ripple @click="$router.replace('chat/1')">
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/avatar3.jpg">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label lines="1">Vika Moll</q-item-label>
+              <q-item-label caption lines="1">
+                Online
+              </q-item-label>
+            </q-item-section>
+
+            <q-item-section side @click="$router.replace('chat/1')">
+              <q-btn size="10px" color="primary" label="Send message"></q-btn>
+            </q-item-section>
+          </q-item>
+
+          <q-separator inset="item"></q-separator>
+
+          <q-item clickable v-ripple @click="$router.replace('chat/1')">
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/avatar4.jpg">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label lines="1">Anton Klai</q-item-label>
+              <q-item-label caption lines="1">
+                Online
+              </q-item-label>
+            </q-item-section>
+
+            <q-item-section side @click="$router.replace('chat/1')">
+              <q-btn size="10px" color="primary" label="Send message"></q-btn>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageFriends'
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
+      { path: 'friends/', component: () => import('pages/Friends.vue') },
       { path: 'message/', component: () => import('pages/Message.vue') },
       { path: 'chat/:id', component: () => import('pages/Chat.vue') }
     ]
